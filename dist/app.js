85c88696df958dac01e6d4fd6c5d22916e025997
Alexander Morse <Sakeran@gmail.com>
2017-02-20 14:59:22 -0500

    Add tests, refactor JS

--- dist/app.js
+++ dist/app.js
@@ -1,32 +1,38 @@
-"use strict";var GAME={colors:["green","red","blue","yellow"],pauseInput:true,// Basic Game Logic
+"use strict";var GAME={colors:["green","red","blue","yellow"],pauseInput:true,gameWin:false,gameLoss:false,// Basic Game Logic
 setupGame:function setupGame(){var strictMode=arguments.length>0&&arguments[0]!==undefined?arguments[0]:false;// Resets the game object and initializes a new game.
-this.strictMode=strictMode;this.sequence=[];for(var i=0;i<20;i++){var newColor=this.colors[Math.floor(Math.random()*this.colors.length)];this.sequence.push(newColor)}this.playerLevel=1;this.playerSequence=[];this.pauseInput=false;this.setLevelField();this.setStrictField()},checkInput:function checkInput(inputArray){// Checks that the input array matches the current sequence up to
+this.strictMode=this.strictMode||strictMode;this.sequence=[];for(var i=0;i<20;i++){var newColor=this.colors[Math.floor(Math.random()*this.colors.length)];this.sequence.push(newColor)}this.gameWin=false;this.gameLoss=false;this.playerLevel=1;this.playerSequence=[];this.pauseInput=false;this.setLevelField();this.setStrictField()},checkInput:function checkInput(inputArray){// Checks that the input array matches the current sequence up to
 // its length. Returns true if it matches, else returns false.
-for(var i=0,len=inputArray.length;i<len;i++){if(inputArray[i]===this.sequence[i]){continue}return false}return true},input:function input(color){// Validates input before adding it on to (a copy of) the user sequence.
-// Then checks the input against the current game sequence.
-// Updates the playerLevel and playerSequence if it matches, otherwise
-// returns false.
-if(!this.colors.includes(color)){return false}var newSeq=this.playerSequence.slice(0);newSeq.push(color);if(this.checkInput(newSeq)){this.playerLevel++;this.playerSequence=newSeq;return true}return false},// Game/DOM Interface
+for(var i=0,len=inputArray.length;i<len;i++){if(inputArray[i]===this.sequence[i]){continue}return false}return true},resetPlayerInput:function resetPlayerInput(){// Clears the player's input array.
+this.playerSequence=[]},input:function input(color){// Validate input before adding it on to (a copy of) the user sequence.
+// Then checks the input against the current game sequence, and updates
+// the game state according to whether the input was valid or not.
+// Returns true if the input was correct/valid, or false if it was not.
+if(!this.colors.includes(color)){throw new Error("Tried to input an invalid color: "+color);return}var newSeq=this.playerSequence.slice(0);newSeq.push(color);if(this.checkInput(newSeq)){// The input was correct. Check to see how long the user's input array is.
+// If it is as long as the current player level, increase player level.
+this.playerSequence=newSeq;if(this.playerSequence.length>=this.playerLevel){console.log("Increasing level");this.increasePlayerLevel()}return true}else{// The input was not correct. Call the functionality to determine the
+// game's fail state.
+this.doFailureState();return false}},increasePlayerLevel:function increasePlayerLevel(){// Increases the player level by one.
+// Calls a victory state and resets the game if the level has
+// reached twenty.
+this.playerLevel++;this.resetPlayerInput();if(this.playerLevel>=20){console.log("You win! Resetting the game.");this.gameWin=true}},doFailureState:function doFailureState(){// Determine the penalty for an incorrect input. If strict mode is on,
+// reset the game entirely. Otherwise, just clear the player's input
+// array.
+if(this.strictMode){this.setupGame();return}this.resetPlayerInput()},// Game/DOM Interface
 init:function init(){// Captures relevant DOM elements, and sets up listeners.
 var colorButtons=document.querySelectorAll(".button");for(var i=0,len=colorButtons.length;i<len;i++){var btn=colorButtons[i];btn.addEventListener("mousedown",this.colorInput.bind(this,btn.id))}},setLevelField:function setLevelField(){var level=this.playerLevel||0;document.querySelector("#info-level").innerHTML=level},setStrictField:function setStrictField(){var res="Strict Mode "+(this.strictMode?"On":"Off");document.querySelector("#info-strict").innerHTML=res},// Input Handling
 colorInput:function colorInput(input){// Don't allow input if the game is currently paused.
-if(this.pauseInput){return}console.log(input);var correct=this.input(input);// If the result was valid (correct), call inputSucceed()
+// Returns true if the input was sucessful.
+if(this.pauseInput){return false}this.animateButton(input);console.log(input);var correct=this.input(input);// If the result was valid (correct), call inputSucceed()
 // Otherwise, play the failure sound and call inputFail()
-if(correct){this.inputSucceed(input)}else{this.inputFail()}},inputSucceed:function inputSucceed(input){// Play the sound effect corresponding to the button pressed.
-console.log("Correct! Sound effect for "+input+" goes here.");// If the user's input array is as long as the entire sequence (20),
-// display a win message and reset the game.
-var userLen=this.playerSequence.length;if(userLen==20){console.log("You win! Win sequence goes here.");return}// If the user's input array is as long as the current level, clear the
-// input array, increase the level by one, and play the pattern animation
-// out.
-if(userLen==this.playerLevel){console.log("You beat this level! Increasing the level...");this.playerLevel++;this.playerSequence=[];console.log("Playing the level animation.")}// Otherwise, do nothing else and wait for more input.
-},inputFail:function inputFail(){// Play the failure sound effect. If in strict mode, reset the game.
-// Otherwise, play the pattern animation up to the current level and reset
-// the user's input array.
-console.log("Failure!");if(this.strictMode){console.log("You are in strict mode! Resetting the game.");return}else{console.log("You got it wrong! Replaying the level animation.");this.playerSequence=[]}},// Sound and animation
+if(correct){this.inputSucceed(input);return true}else{this.inputFail();return false}},inputSucceed:function inputSucceed(input){// Play the sound effect corresponding to the button pressed.
+console.log("Correct! Sound effect for "+input+" goes here.")},inputFail:function inputFail(){// Play the failure sound effect.
+console.log("Failure! Sound effect for failure goes here.")},// Sound and animation
 animateButton:function animateButton(color){// Plays the activation animation for the button of the given color.
-var target=document.querySelector("."+color);if(!target){return}var baseClasses="button "+color;var classes=target.getAttribute("class").split(" ");if(!classes.includes("active")){classes.push("active")}else{// If the class is already set, remove it before doing anything else
-target.setAttribute("class",baseClasses)}target.setAttribute("class",classes.join(" "));// Clear the "active" class after a short interval.
-setTimeout(function(){target.setAttribute("class",baseClasses)},600)},playSequence:function playSequence(level){// Animates each button in the game sequence, up to the level specified.
+var target=document.querySelector("."+color);if(!target){return}var baseClasses="button "+color;var classes=target.getAttribute("class").split(" ");if(!classes.includes("active")){classes.push("active")}else{// If the class is already set, we'll want to restart the animation.
+// Since we're using CSS keyframes for this, this means removing the
+// target entirely and replacing it with a clone.
+var newTarget=target.cloneNode(true);newTarget.setAttribute("class",baseClasses);newTarget.addEventListener("mousedown",this.colorInput.bind(this,target.id));target.parentNode.replaceChild(newTarget,target);target=newTarget}target.setAttribute("class",classes.join(" "));// Clear the "active" class after a short interval.
+setTimeout(function(){if(target.getAttribute("class")==baseClasses){return}target.setAttribute("class",baseClasses)},600)},playSequence:function playSequence(level){// Animates each button in the game sequence, up to the level specified.
 // Plays the entire animation if no level is given.
 // Returns a promise that resolves after each step of the animation has
 // played.
--- tests/tests.js
+++ tests/tests.js
@@ -1,23 +1,58 @@
-QUnit.test("Sequence Matching(checkInput(),input())", function(assert) {
+function getWrongColor(color) {
+  return (color == "blue") ? "red" : "blue";
+}
+
+QUnit.test("Sequence matching", function(assert) {
   GAME.setupGame();
   var copySequence = GAME.sequence.slice(0);
   assert.ok(GAME.checkInput(copySequence),"Validates correct input sequences.");
-  var wrongInput =  (copySequence[1] == "blue") ? "red" : "blue";
+  var wrongInput =  getWrongColor(copySequence[1])
   copySequence[1] = wrongInput;
   assert.notOk(GAME.checkInput(copySequence), "Rejects invalid sequences");
-  copySequence[1] = GAME.sequence[1];
-  assert.ok(GAME.input(copySequence[0]),"Accepts correct input");
-  assert.equal(GAME.playerLevel, 2, "Increases player level");
-  assert.equal(GAME.playerSequence.length, 1, "Adds to current player sequnce.");
-  assert.notOk(GAME.input(wrongInput),"Rejects incorrect input.");
+});
+
+QUnit.test("Sequence/Level mechanics", function(assert) {
+  GAME.setupGame();
+  assert.equal(GAME.sequence.length, 20, "Twenty colors in sequence queue");
+  assert.equal(GAME.playerLevel, 1, "Player level starts at 1");
+  assert.notOk(GAME.strictMode, "Strict mode is off by default");
+  assert.ok(GAME.input(GAME.sequence[0]), "Accepts a single correct input");
+  assert.equal(GAME.playerLevel, 2, "Player level increases properly.");
+  assert.equal(GAME.playerSequence.length, 0, "Player entries update properly.");
+
+  var wrong = GAME.input(getWrongColor(GAME.sequence[0]));
+  assert.notOk(wrong,"Fails on a single incorrect input");
+  assert.equal(GAME.playerSequence.length, 0, "Player sequence resets on failure.");
+  assert.equal(GAME.playerLevel, 2, "Player level is unchanged on failure.")
+  GAME.strictMode = true;
+  var initialSequence = GAME.sequence;
+  console.log(initialSequence);
+  GAME.input(getWrongColor(GAME.sequence[0]));
+  console.log(GAME.sequence);
+  assert.notEqual(GAME.sequence,initialSequence, "New colors generated on fail"
+  + " in strict mode.");
+  assert.equal(GAME.playerLevel, 1, "Player level resets on strict mode fail.");
   var allValid = true;
-  GAME.playerSequence = [];
-  GAME.playerLevel = 1;
-  for (var i = 0; i < copySequence.length; i++) {
-    if (!GAME.input(copySequence[i])) {
+  GAME.setupGame();
+  GAME.playerLevel = 20;
+  for (var i = 0; i < GAME.sequence.length; i++) {
+    if (!GAME.input(GAME.sequence[i])) {
       allValid = false;
+      console.log("Invalid: "+GAME.sequence[i]);
       break;
     }
   }
   assert.ok(allValid,"Can validate a whole game");
+});
+
+QUnit.test("Color button input", function(assert) {
+  GAME.setupGame();
+  GAME.pauseInput = true;
+  var correct = GAME.sequence[0];
+  assert.notOk(GAME.colorInput(correct), "Ignores input while paused.");
+  GAME.playerSequence = [];
+  GAME.pauseInput = false;
+  assert.ok(GAME.colorInput(correct), "Calls succeeed() correctly");
+  var wrongInput =  getWrongColor(GAME.sequence[1]);
+  assert.notOk(GAME.colorInput(wrongInput),"Calls failure() correctly");
 });
