77a46e6e7bc49ed1df57ea5a7b7e1fa2c4bb3200
Alexander Morse <Sakeran@gmail.com>
2017-02-21 15:18:37 -0500

    Add promise polyfill, fix compatibility issue

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,6 @@
 "use strict";var GAME={colors:["green","red","blue","yellow"],pauseInput:true,// Basic Game Logic
 setupGame:function setupGame(){// Resets the game object and initializes a new game.
-this.sequence=[];for(var i=0;i<20;i++){var newColor=this.colors[Math.floor(Math.random()*this.colors.length)];this.sequence.push(newColor)}this.playerLevel=1;this.playerSequence=[];this.setLevelField();this.setStrictField();this.pauseInput=true;this.playCurrentLevel()},playCurrentLevel:function playCurrentLevel(){// Sets pauseInput to true while the current level sequence plays,
+this.sequence=[];for(var i=0;i<20;i++){var newColor=this.colors[Math.floor(Math.random()*this.colors.length)];this.sequence.push(newColor)}this.playerLevel=1;this.playerSequence=[];this.pauseInput=true;this.playCurrentLevel()},playCurrentLevel:function playCurrentLevel(){// Sets pauseInput to true while the current level sequence plays,
 // then sets it back to false to allow input.
 // Should also set the UI components at this stage.
 this.setLevelField();this.setStrictField();this.pauseInput=true;// Wait one second before beginning the animation.
@@ -11,7 +11,7 @@ this.playerSequence=[]},input:function input(color){// Validate input before add
 // Then checks the input against the current game sequence, and updates
 // the game state according to whether the input was valid or not.
 // Returns true if the input was correct/valid, or false if it was not.
-if(!this.colors.includes(color)){throw new Error("Tried to input an invalid color: "+color);return}var newSeq=this.playerSequence.slice(0);newSeq.push(color);if(this.checkInput(newSeq)){// The input was correct. Check to see how long the user's input array is.
+if(this.colors.indexOf(color)==-1){throw new Error("Tried to input an invalid color: "+color);return}var newSeq=this.playerSequence.slice(0);newSeq.push(color);if(this.checkInput(newSeq)){// The input was correct. Check to see how long the user's input array is.
 // If it is as long as the current player level, increase player level.
 this.playerSequence=newSeq;if(this.playerSequence.length>=this.playerLevel){console.log("Increasing level");this.increasePlayerLevel()}return true}else{// The input was not correct. Call the functionality to determine the
 // game's fail state.
@@ -23,7 +23,7 @@ this.playerLevel++;this.resetPlayerInput();if(this.playerLevel>=20){console.log(
 // array.
 if(this.strictMode){this.setupGame();return}this.resetPlayerInput();this.playCurrentLevel()},// Game/DOM Interface
 init:function init(){// Captures relevant DOM elements, and sets up listeners.
-var colorButtons=document.querySelectorAll(".button");for(var i=0,len=colorButtons.length;i<len;i++){var btn=colorButtons[i];btn.addEventListener("mousedown",this.colorInput.bind(this,btn.id))}},setLevelField:function setLevelField(){var level=this.playerLevel||0;document.querySelector("#info-level").innerHTML=level},setStrictField:function setStrictField(){var res="Strict Mode "+(this.strictMode?"On":"Off");document.querySelector("#info-strict").innerHTML=res},// Input Handling
+var colorButtons=document.querySelectorAll(".button");for(var i=0,len=colorButtons.length;i<len;i++){var btn=colorButtons[i];btn.addEventListener("touchstart",this.colorInput.bind(this,btn.id));btn.addEventListener("mousedown",this.colorInput.bind(this,btn.id))}},setLevelField:function setLevelField(){var level=this.playerLevel||0;document.querySelector("#info-level").innerHTML=level},setStrictField:function setStrictField(){var res="Strict Mode "+(this.strictMode?"On":"Off");document.querySelector("#info-strict").innerHTML=res},// Input Handling
 colorInput:function colorInput(input){// Don't allow input if the game is currently paused.
 // Returns true if the input was sucessful.
 if(this.pauseInput){return false}this.animateButton(input);console.log(input);var correct=this.input(input);// If the result was valid (correct), call inputSucceed()
@@ -33,7 +33,7 @@ console.log("Correct! Sound effect for "+input+" goes here.")},inputFail:functio
 console.log("Failure! Sound effect for failure goes here.")},// Sound and animation
 animateButton:function animateButton(color){// Plays the activation animation for the button of the given color.
 // Returns a promise that resolves after the animation has played out
-var target=document.querySelector("."+color);if(!target){return}var baseClasses="button "+color;var classes=target.getAttribute("class").split(" ");if(!classes.includes("active")){classes.push("active")}else{// If the class is already set, we'll want to restart the animation.
+var target=document.querySelector("."+color);if(!target){return}var baseClasses="button "+color;var classes=target.getAttribute("class").split(" ");if(classes.indexOf("active")==-1){classes.push("active")}else{// If the class is already set, we'll want to restart the animation.
 // Since we're using CSS keyframes for this, this means removing the
 // target entirely and replacing it with a clone.
 var newTarget=target.cloneNode(true);newTarget.setAttribute("class",baseClasses);newTarget.addEventListener("mousedown",this.colorInput.bind(this,target.id));target.parentNode.replaceChild(newTarget,target);target=newTarget}target.setAttribute("class",classes.join(" "));return new Promise(function(resolve,reject){// Clear the "active" class after a short interval.
@@ -42,4 +42,4 @@ setTimeout(function(){if(target.getAttribute("class")==baseClasses){return}targe
 // Returns a promise that resolves after each step of the animation has
 // played.
 // Speed parameter is given in seconds, and converted to ms.
-level=level||sequence.length;speed=Math.floor(speed*1000)||1000;var game=this;var sequence=this.sequence;var animations=[];var _loop=function _loop(i){animations.push(new Promise(function(resolve,reject){setTimeout(function(){game.animateButton(sequence[i]).then(function(){resolve()})},speed*i)}))};for(var i=0;i<level;i++){_loop(i)}var res=Promise.all(animations);return res}};GAME.init();GAME.setupGame();
+var sequence=this.sequence;level=level||sequence.length;speed=Math.floor(speed*1000)||1000;var game=this;var animations=[];var _loop=function _loop(i){animations.push(new Promise(function(resolve,reject){setTimeout(function(){game.animateButton(sequence[i]).then(function(){resolve()})},speed*i)}))};for(var i=0;i<level;i++){_loop(i)}var res=Promise.all(animations);return res}};GAME.init();GAME.setupGame();
